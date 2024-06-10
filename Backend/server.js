2190b043c64eaa83c8a8dbafa631204e8cad4824
ANKIT MISHRA <148037556+ankit938@users.noreply.github.com>
2024-06-10 22:34:56 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,10 @@ const app=require('./app.js')
 
 
 
-const url='mongodb+srv://$_USERNAME_$:$_PASSWORD_$@cluster0.dqcim5j.mongodb.net/$_DB_NAME_$?retryWrites=true&w=majority&appName=Cluster0'
+// const url='mongodb+srv://$_USERNAME_$:$_PASSWORD_$@cluster0.dqcim5j.mongodb.net/$_DB_NAME_$?retryWrites=true&w=majority&appName=Cluster0'
+   const url='mongodb+srv://$_USERNAME_$:$_PASSWORD_$@cluster0.dqcim5j.mongodb.net/$_DB_NAME_$?retryWrites=true&w=majority&appName=Cluster0'
 const databaseUser='ankitiaf2002';
-const databasePassword='ankit12345';
+const databasePassword='ankit123';
 const databaseName='Image-generator';
 
 let dbLink=url.replace("$_USERNAME_$",databaseUser);
